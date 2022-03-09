7c69ffa593c8cafd182a4a201a31f0bbc3baf205
Arslan Abdullaev <ars352@yahoo.com>
2022-03-09 17:02:08 +0300

    Initial Commit

---
+++ HW7_SwiftBook/HW7_SwiftBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW7_SwiftBookApp.swift
+//  HW7_SwiftBook
+//
+//  Created by Arslan Abdullaev on 09.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct HW7_SwiftBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
